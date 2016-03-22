3ee58f17144385b9820a97479c60cc2df3d0f69d
Sarah Harris <Sarah Harris>
2016-03-22 16:19:15 -0400

    MainScreen 3.22.16

---
+++ WebContent/MainScreen.js
@@ -0,0 +1,19 @@
+
+  function topPets(){
+	  var e = document.getElementById("type-Of-Pet");
+	  if(e.value == "Cat"){
+	  title=e;
+	  window.location.href='login.html';
+	  }
+	  
+	  }
+  
+  function recentUploaded(){
+	  var e = document.getElementById("type-Of-Pet");
+	  if(e.value == "Cat"){
+	  title=e;
+	  window.location.href='login.html';
+	  }
+	  
+	  }
+
