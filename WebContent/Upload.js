b65042d9c13b32b4d31e730d588e99dfd37b8a11
jsjohn23 <jsjohn23@uncg.edu>
2016-03-26 16:05:30 -0400

    Sarah 03.26.16.01

---
+++ WebContent/Upload.js
@@ -0,0 +1,22 @@
+// Photo Upload
+
+$(function () {
+    $(":file").change(function () {
+        if (this.files && this.files[0]) {
+            var reader = new FileReader();
+            reader.onload = imageIsLoaded;
+            reader.readAsDataURL(this.files[0]);
+        }
+    });
+});
+
+function imageIsLoaded(e) {
+    $('#myImg').attr('src', e.target.result);
+};
+
+
+//Combo Box
+
+$('#selectPetName').scombobox();
+
+$('#selectPetCategory').scombobox();
